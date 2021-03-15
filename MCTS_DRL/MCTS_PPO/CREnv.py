6c4e85082c5b49dc924a234aff027d9c09ccf92b
ybiao-he <ybiao2016@gmail.com>
2021-03-15 17:08:33 -0500

    initial code for MCTS and PPO, the MCTS env is finished

---
+++ MCTS_DRL/MCTS_PPO/CREnv.py
@@ -0,0 +1,349 @@
+from __future__ import division
+
+from copy import copy
+from copy import deepcopy
+from scipy.spatial import distance
+
+import numpy as np
+import gym
+from gym import spaces
+import os, random
+
+
+class CREnv(gym.Env):
+    """
+    Custom Environment that follows gym interface.
+    This is an env for circuit routing. 
+    """
+    def __init__(self):
+        super(CREnv, self).__init__()
+
+        self.directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+        self.state_shape = (4,)
+
+        n_actions = 4
+        self.action_space = spaces.Discrete(n_actions)
+        self.observation_space = spaces.Box(low=0, high=30, shape=self.state_shape, dtype=np.float32)
+
+    def reset(self):
+
+        self.board = np.genfromtxt("./board.csv", delimiter=',')
+        # read board form dir
+        # directory = './boards_30_30'
+        # filename = random.choice(os.listdir(directory))
+        # board_rand = os.path.join(directory, filename)
+        # self.board = np.genfromtxt(board_rand, delimiter=',')
+
+        self.head_value = 20
+        
+        self.path_length = 0
+
+        self.max_pair = int(np.amax(self.board))
+        # self.max_pair = 3
+        self.connection = False
+        self.collide = False
+
+        # parse the board and get the starts and ends
+        self.start = {}
+        self.finish = {}
+        for i in range(self.board.shape[0]):
+            for j in range(self.board.shape[1]):
+                if abs(self.board[i,j])>=2:
+                    if abs(self.board[i,j])<=self.max_pair:
+                        if self.board[i,j]<0:
+                            self.finish[-self.board[i,j]] = (i,j)
+                            self.board[i,j] = abs(self.board[i,j])
+                        else:
+                            self.start[self.board[i,j]] = (i,j)
+                    else:
+                        self.board[i,j] = 0
+                # self.board[i,j] = abs(self.board[i,j])
+
+        # initialize the action node
+        self.pairs_idx = int(min(self.start.keys()))
+        self.action_node = self.start[self.pairs_idx]
+
+        self.board[self.action_node] = self.head_value
+        # print(self.start, self.finish)
+
+        state = self.board_embedding()
+        return state
+
+    def step(self, action):
+
+        action_tmp = self.directions[action]
+        self.connection = False
+        self.collide = False
+        self.action_node_pre = self.action_node
+
+        self.path_length += 1
+
+        # pre-determine new action node
+        self.action_node = (self.action_node[0]+action_tmp[0], self.action_node[1]+action_tmp[1])
+        # check/adjust new action node and set its value
+        x = self.action_node[0]
+        y = self.action_node[1]
+
+        if 0 <= x < self.board.shape[0] and 0 <= y < self.board.shape[1]:
+            if self.action_node == self.finish[self.pairs_idx]:
+                self.goto_new_net(True)
+            elif self.board[self.action_node]!=0:
+                self.collide = True
+                self.goto_new_net(False)
+            else:
+                self.board[self.action_node_pre] = 1
+                self.board[self.action_node] = self.head_value
+        else:
+            self.collide = True
+            self.goto_new_net(False, out_range=True)
+
+        reward = self.getReward()
+
+        state = self.board_embedding()
+
+        done = self.isTerminal()
+
+        info = {}
+
+        return state, reward, done, info
+
+    def goto_new_net(self, connection_sign, out_range=False):
+
+        self.board[self.action_node_pre] = 1
+        self.connection = connection_sign
+        self.pairs_idx += 1
+        if not out_range:
+            self.board[self.action_node] = 1
+            self.action_node_pre = self.action_node
+        if self.pairs_idx<=self.max_pair:
+            self.action_node = self.start[self.pairs_idx]
+            self.board[self.action_node] = self.head_value
+
+    def isTerminal(self):
+
+        if self.pairs_idx>self.max_pair:
+            return True
+
+        return False
+
+
+    def getReward(self):
+
+        if self.connection:
+            return 20
+        if self.collide:
+            left_dist = 5*distance.cityblock(self.action_node_pre, self.finish[self.pairs_idx-1])
+            return -left_dist/10
+
+        return -0.1
+
+    def getPossibleActions(self):
+
+        possible_actions = []
+        for d in self.directions:
+            x = self.action_node[0] + d[0]
+            y = self.action_node[1] + d[1]
+            if 0 <= x < self.board.shape[0] and 0 <= y < self.board.shape[1]:
+                if (x,y) == self.finish[self.pairs_idx]:
+                    possible_actions = [(d[0], d[1])]
+                    break
+                elif self.board[(x,y)] == 0:
+                    possible_actions.append((d[0], d[1]))
+
+        ret = []
+        for i in range(len(possible_actions)):
+            ret.append(self.directions.index(possible_actions[i]))
+        return ret
+
+    def compute_mask(self):
+        possible_acts = self.getPossibleActions()
+        mask = np.zeros(self.action_space.n)
+        for i in possible_acts:
+            mask[i] = 1
+        if len(possible_acts)==0:
+            mask = np.ones(self.action_space.n)
+        return mask
+
+    def board_embedding(self):
+
+        if self.pairs_idx<=self.max_pair:
+            dist_to_target = [i-j for i, j in zip(self.action_node, self.finish[self.pairs_idx])]
+        else:
+            dist_to_target = [i-j for i, j in zip(self.action_node, self.finish[self.pairs_idx-1])]
+        # state = np.array(list(self.action_node)+list(self.finish[self.pairs_idx]))
+        state = np.array(list(self.action_node)+dist_to_target)
+        # state = np.concatenate(( state, np.array(nets_vector.tolist()+obs_vector.tolist()) ), axis=0)
+
+        return state
+
+    def render(self, mode='console'):
+        if mode != 'console':
+            raise NotImplementedError()
+
+        print("------")
+        print(self.action_node)
+
+    def close(self):
+        pass
+
+
+
+####    Environment for MCTS    ####
+
+class CREnv_MCTS():
+    def __init__(self):
+        super(CREnv_MCTS, self).__init__()
+
+        self.directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+        self.state_shape = (4,)
+
+        self.n_actions = 4
+
+    def reset(self):
+
+        self.board = np.genfromtxt("./board.csv", delimiter=',')
+        # read board form dir
+        # directory = './boards_30_30'
+        # filename = random.choice(os.listdir(directory))
+        # board_rand = os.path.join(directory, filename)
+        # self.board = np.genfromtxt(board_rand, delimiter=',')
+
+        self.head_value = 20
+        
+        self.path_length = 0
+
+        self.max_pair = int(np.amax(self.board))
+        # self.max_pair = 3
+        self.connection = False
+        self.collide = False
+
+        # parse the board and get the starts and ends
+        self.start = {}
+        self.finish = {}
+        for i in range(self.board.shape[0]):
+            for j in range(self.board.shape[1]):
+                if abs(self.board[i,j])>=2:
+                    if abs(self.board[i,j])<=self.max_pair:
+                        if self.board[i,j]<0:
+                            self.finish[-self.board[i,j]] = (i,j)
+                            self.board[i,j] = abs(self.board[i,j])
+                        else:
+                            self.start[self.board[i,j]] = (i,j)
+                    else:
+                        self.board[i,j] = 0
+                # self.board[i,j] = abs(self.board[i,j])
+
+        # initialize the action node
+        self.pairs_idx = int(min(self.start.keys()))
+        self.action_node = self.start[self.pairs_idx]
+
+        self.board[self.action_node] = self.head_value
+
+        self.total_reward = 0
+
+    def takeAction(self, action):
+
+        newState = deepcopy(self)
+
+        action_tmp = newState.directions[action]
+        newState.connection = False
+        newState.collide = False
+        newState.action_node_pre = newState.action_node
+
+        newState.path_length += 1
+
+        # pre-determine new action node
+        newState.action_node = (newState.action_node[0]+action_tmp[0], newState.action_node[1]+action_tmp[1])
+        # check/adjust new action node and set its value
+        x = newState.action_node[0]
+        y = newState.action_node[1]
+
+        if 0 <= x < newState.board.shape[0] and 0 <= y < newState.board.shape[1]:
+            if newState.action_node == newState.finish[newState.pairs_idx]:
+                newState.goto_new_net(True)
+            elif newState.board[newState.action_node]!=0:
+                newState.collide = True
+                newState.goto_new_net(False)
+            else:
+                newState.board[newState.action_node_pre] = 1
+                newState.board[newState.action_node] = newState.head_value
+        else:
+            newState.collide = True
+            newState.goto_new_net(False, out_range=True)
+
+        newState.total_reward += newState.step_reward()
+
+        return newState, newState.isTerminal()
+
+    def goto_new_net(self, connection_sign, out_range=False):
+
+        self.board[self.action_node_pre] = 1
+        self.connection = connection_sign
+        self.pairs_idx += 1
+        if not out_range:
+            self.board[self.action_node] = 1
+            self.action_node_pre = self.action_node
+        if self.pairs_idx<=self.max_pair:
+            self.action_node = self.start[self.pairs_idx]
+            self.board[self.action_node] = self.head_value
+
+    def isTerminal(self):
+
+        if self.pairs_idx>self.max_pair:
+            return True
+
+        return False
+
+    def getReward(self):
+        return self.total_reward
+
+    def step_reward(self):
+
+        if self.connection:
+            return 20
+        if self.collide:
+            left_dist = 5*distance.cityblock(self.action_node_pre, self.finish[self.pairs_idx-1])
+            return -left_dist/10
+
+        return -0.1
+
+    def getPossibleActions(self):
+
+        possible_actions = []
+        for d in self.directions:
+            x = self.action_node[0] + d[0]
+            y = self.action_node[1] + d[1]
+            if 0 <= x < self.board.shape[0] and 0 <= y < self.board.shape[1]:
+                if (x,y) == self.finish[self.pairs_idx]:
+                    possible_actions = [(d[0], d[1])]
+                    break
+                elif self.board[(x,y)] == 0:
+                    possible_actions.append((d[0], d[1]))
+
+        ret = []
+        for i in range(len(possible_actions)):
+            ret.append(self.directions.index(possible_actions[i]))
+        return ret
+
+    def compute_mask(self):
+        possible_acts = self.getPossibleActions()
+        mask = np.zeros(self.n_actions)
+        for i in possible_acts:
+            mask[i] = 1
+        if len(possible_acts)==0:
+            mask = np.ones(self.n_actions)
+        return mask
+
+    def board_embedding(self):
+
+        if self.pairs_idx<=self.max_pair:
+            dist_to_target = [i-j for i, j in zip(self.action_node, self.finish[self.pairs_idx])]
+        else:
+            dist_to_target = [i-j for i, j in zip(self.action_node, self.finish[self.pairs_idx-1])]
+        # state = np.array(list(self.action_node)+list(self.finish[self.pairs_idx]))
+        state = np.array(list(self.action_node)+dist_to_target)
+        # state = np.concatenate(( state, np.array(nets_vector.tolist()+obs_vector.tolist()) ), axis=0)
+
+        return state
---
+++ MCTS_DRL/MCTS_PPO/animate.py
@@ -0,0 +1,135 @@
+import numpy as np
+import tensorflow as tf
+from config import Params
+from wrapper_env import env_wrapper
+import gym
+
+from nn_architecures import network_builder
+from models import CategoricalModel, GaussianModel
+from policy import Policy
+from CREnv import CREnv
+from copy import copy
+import os
+
+from CREnv import CREnv_MCTS
+
+def draw_board(paths_x, paths_y, board, save_name):
+    
+    import matplotlib.pyplot as plt
+    width, height = board.shape
+
+    # create a 8" x 8" board
+    fig = plt.figure(figsize=[8,8])
+    # fig.patch.set_facecolor((1,1,.8))
+
+    ax = fig.add_subplot(111)
+
+    # draw the grid
+    for x in range(30):
+        ax.plot([x, x], [0,29], color=(0.5,0.5,0.5,1))
+    for y in range(30):
+        ax.plot([0, 29], [y,y], color=(0.5,0.5,0.5,1))
+
+    # draw paths
+    for p in range(len(paths_x)):
+
+        ph = plt.subplot()
+        ph.plot(paths_y[p], paths_x[p], linewidth=5, color='black')
+
+    # draw obstacles
+    x_axis = []
+    y_axis = []
+    nets = dict()
+    for x in range(width):
+        for y in range(height):
+            if board[x, y]!=0:
+                x_axis.append(y)
+                y_axis.append(x)
+                if board[x, y]!=1:
+                    nets[(x,y)] = board[x, y]
+
+    ax.scatter(x_axis, y_axis, marker='s', s=250, c='k')
+
+    for xy in nets:
+        ax.text(xy[1], xy[0], str(int(nets[xy])-1), fontsize=18, color='w',
+                horizontalalignment='center', verticalalignment='center')
+
+    # scale the axis area to fill the whole figure
+    ax.set_position([0,0,1,1])
+
+    # get rid of axes and everything (the figure background will show through)
+    ax.set_axis_off()
+
+    # scale the plot area conveniently (the board is in 0,0..18,18)
+    ax.set_xlim(0,29)
+    ax.set_ylim(0,29)
+    
+    fig.savefig(save_name, bbox_inches='tight')
+
+def rollout(env, model, res_idx):
+
+    res_folder_name = "route_results"
+    if not os.path.isdir(res_folder_name):
+        os.mkdir(res_folder_name)
+    
+    saved_fig_name = os.path.join(res_folder_name, "route_board_{}.png".format(res_idx))
+
+    env.reset()
+    state = env
+    done = False
+
+    board = np.absolute(np.genfromtxt("./board.csv", delimiter=','))
+
+    paths_x = []
+    paths_y = []
+    path_x = []
+    path_y = []
+    while not done:
+
+        obs_vec = np.expand_dims(state.board_embedding(), axis=0)
+        obs_vis = None
+        path_x.append(obs_vec[0][0])
+        path_y.append(obs_vec[0][1])
+        mask = state.compute_mask()
+        action_t, logp_t, value_t = model.get_action_logp_value({"vec_obs": obs_vec, "vis_obs": obs_vis}, mask=mask)
+
+        print(obs_vec)
+        state, done = state.takeAction(action_t)
+        if state.pairs_idx-2>len(paths_x):
+            paths_x.append(path_x)
+            paths_y.append(path_y)
+            path_x = []
+            path_y = []
+    draw_board(paths_x, paths_y, board, saved_fig_name)
+
+    return state.getReward()
+
+if __name__ == "__main__":
+
+    # physical_devices = tf.config.list_physical_devices('GPU') 
+    # tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    params = Params()          # Get Configuration | HORIZON = Steps per epoch
+
+    tf.random.set_seed(params.env.seed)                                     # Set Random Seeds for np and tf
+    np.random.seed(params.env.seed)
+
+    # env = create_batched_env(params.env.num_envs, params.env)               # Create Environment in multiprocessing mode
+    # env = gym.make('CartPole-v0')
+    env = CREnv()
+    env = env_wrapper(env, params.env)
+
+    network = network_builder(params.trainer.nn_architecure) \
+        (hidden_sizes=params.policy.hidden_sizes, env_info=env.env_info)    # Build Neural Network with Forward Pass
+
+    model = CategoricalModel if env.env_info.is_discrete else GaussianModel
+    model = model(network=network, env_info=env.env_info)                   # Build Model for Discrete or Continuous Spaces
+
+    env_mcts = CREnv_MCTS()
+    # if params.trainer.load_model:
+    print('Loading Model ...')
+    model.load_weights("./saved_model/oneNet_vec_oneHit")           # Load model if load_model flag set to true
+
+    for i in range(50):
+        ep_rew = rollout(env_mcts, model, i)
+        print(ep_rew)
